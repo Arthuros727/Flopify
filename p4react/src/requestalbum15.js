6df64585e589a67923382aad435e5bbed1fb25ad
Guerin <maximan@Tix>
2023-06-07 13:48:18 +0200

    commit auto

--- p4react/src/requestalbum15.js
+++ p4react/src/requestalbum15.js
@@ -1,46 +1,50 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import ReactPaginate from "react-paginate";
 
 function Request15() {
   const [posts, setPosts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const fetchPosts = async () => {
+    try {
+      const response = await axios.get(`http://localhost:8000/albums?page=${currentPage}&limit=10`);
+      setPosts(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   useEffect(() => {
-    axios.get('http://localhost:8000/albums?limit=15')
-      .then(response => {
-        setPosts(response.data);
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  }, []);
+    fetchPosts();
+  }, [currentPage]);
+
+  const handlePreviousPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const handleNextPage = () => {
+    setCurrentPage(currentPage + 1);
+  };
 
   return (
-    <ul>
-      {posts.map(post => (
-        <div className='homepage'>
-          <img src={post.cover_small}></img>
-          <p>{post.name}</p>
-          <p>Popularity : {post.popularity}/100</p>
-          <div>
-        {data.map(() => {
-          return <p>{country.name.common}</p>;
-        })}
-        <ReactPaginate
-          previousLabel={"Previous"}
-          nextLabel={"Next"}
-          breakLabel={"..."}
-          pageCount={25}
-          marginPagesDisplayed={3}
-          pageRangeDisplayed={6}
-          onPageChange={handlePageClick}
-          containerClassName={"pagination"}
-        />
+    <div>
+      <ul>
+        {posts.map(post => (
+          <div className='homepage' key={post.id}>
+            <img src={post.cover_small} alt={post.name} />
+            <p>{post.name}</p>
+            <p>Popularity: {post.popularity}/100</p>
+          </div>
+        ))}
+      </ul>
+
+      <div className='btnPage'>
+        <button onClick={handlePreviousPage}>BACK</button>
+        <button onClick={handleNextPage}>NEXT</button>
       </div>
-        </div>
-        // <li>{post.name}</li>
-      ))}
-    </ul>
+    </div>
   );
 }
 
